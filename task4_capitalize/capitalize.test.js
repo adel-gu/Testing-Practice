4cd2475dad9397a2e339a1776c3b434edbafdc13
adelguit <adeelguitoun@gmail.com>
2022-10-04 23:16:29 +0100

    Write test case for input type of string but it's a number

--- task4_capitalize/capitalize.test.js
+++ task4_capitalize/capitalize.test.js
@@ -2,15 +2,26 @@ const capitalize = require('./capitalize');
 
 describe('Capitalize only string format inputs', () => {
   describe('Checking for string only!', () => {
-    test(`Given 3 should produce Error:"Exepct string format only!"`, () => {
+    test(`Given 3 should produce Error:"Exepct string in letters format only!"`, () => {
       // Arrange
-      const string = 3;
-      const capitalized_string_Error = 'Exepct string format only!';
+      const number = 3;
+      const error = 'Exepct string in letters format only!';
 
       // Act
 
       // Assert
-      expect(() => capitalize(string)).toThrow(capitalized_string_Error);
+      expect(() => capitalize(number)).toThrow(error);
+    });
+
+    test(`Given "3" should produce Error:"Exepct string in letters format only!"`, () => {
+      // Arrange
+      const number_in_string_format = '3';
+      const error = 'Exepct string in letters format only!';
+
+      // Act
+
+      // Assert
+      expect(() => capitalize(number_in_string_format)).toThrow(error);
     });
   });
 });
